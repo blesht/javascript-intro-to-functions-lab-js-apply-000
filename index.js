9e498df4efc733aca7d403a8e24d0b625e7219a6
upbeat-queue-5483 <bryan.lesht@colorado.edu>
2019-12-30 23:41:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,24 +2,3 @@ function shout(string) {
   return string.toUpperCase();
 }
 
-function whisper(string) {
-  return string.toLowerCase();
-}
-function logShout(string) {
-  string = string.toUpperCase();
-  console.log(string);
-  
-}
-logWhisper(string) {
-  string=string.toLowerCase();
-  console.log(string);
-}
-
-function sayHiToGrandma(string) {if (string === string.toUpperCase()) { 
-	response = 'YES INDEED';}
-	else if (string === "I love you, Grandma.") {
-	  response = 'I love you, too.'
-	};
-    else { response = "I can't hear you!" };
-  return response;
-}
